00929122e51d778073c7e12dc8fbda2b75a9ec2a
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-01-18 20:29:27 -0500

    Files for Lab 2

---
+++ proxy_server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import socket
+import time
+import sys
+
+# define address & buffer size
+HOST = ""
+PORT = 8001
+BUFFER_SIZE = 1024
+
+
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbytename(host)
+    except socket.gaierror:
+        print('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print(f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+
+def main():
+    host = 'www.google.com'
+    port = 80
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start:
+        print("Starting proxy server")
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST, PORT))
+        proxy_start.listen(2)
+
+        # continuously listen for connections
+        while True:
+            conn, addr = proxy_start.accept()
+            print("Connected by", addr)
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
+                print("Connecting to Google")
+                remote_ip = get_remote_ip(host)
+
+                proxy_end.connect((remote_ip, port))
+
+                send_full.data = conn.recv(BUFFER_SIZE)
+                print(f"Sending received data {send_full_data} to google")
+                proxy_end.sendall(send_full_data)
+
+                proxy_end.shutdown(socket.SHUT_WR)
+
+
+if __name__ == "__main__":
+    main()
